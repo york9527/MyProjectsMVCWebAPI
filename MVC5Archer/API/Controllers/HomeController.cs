dafa5a476231920ec2fd4188062fd0ff2c425787
york9527 <villagearcher@gmail.com>
2014-11-03 15:20:33 +0800

    add web api

---
+++ MVC5Archer/API/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MVC5Archer.API.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "homeapi";
+        }
+    }
+}
